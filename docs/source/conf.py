629836cc4c856211383ac7293d27c52bbe1f0033
Phurailatpam Hemantakumar <hemantaphurailatpam@gmail.com>
2023-05-08 21:40:20 +0800

    from git

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,11 +12,11 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
+    "myst_nb",
+    "sphinx_book_theme"
 ]
 
 intersphinx_mapping = {
